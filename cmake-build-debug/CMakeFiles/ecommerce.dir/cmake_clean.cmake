6c0ad0614915a85298f0e54888fa7105070cb1c8
LASRIDOR <35655062+LASRIDOR@users.noreply.github.com>
2020-01-09 13:35:10 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/ecommerce.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecommerce.dir/main.cpp.o"
+  "ecommerce"
+  "ecommerce.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ecommerce.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
